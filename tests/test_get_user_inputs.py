e2e018b926901f89a8a603de7ab64318e3c14df1
Rubén Crespo Blanco <rcresb@gmail.com>
2025-02-14 20:15:58 +0100

    Added the tests for ensure_output_directory, and the gitignore.

---
+++ tests/test_get_user_inputs.py
@@ -0,0 +1,17 @@
+import pytest
+from ecoliframalpha.input_handler import get_user_inputs
+
+"""
+Unfinished test
+
+def test_function_output():
+    assert module1.some_function(5) == 25
+
+def test_edge_case():
+    assert module1.some_function(0) == 0
+
+def test_raises_error():
+    with pytest.raises(ValueError):
+        module1.some_function("invalid_input")
+
+"""
